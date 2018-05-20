852de237e5ad5cd1ac907d8000edcc5e94bdb35a
albertop10 <albertop@hotmail.es>
2018-05-21 01:12:47 +0200

    Conexion+librerias

---
+++ ProyectoLuca/src/util/Conexion.java
@@ -0,0 +1,42 @@
+package util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Conexion {
+
+	
+	private static Connection con = connectDB();
+	
+	public static Connection connectDB() {
+		try {
+			String driverClassName = "com.mysql.jdbc.Driver";
+			String driverUrl = "jdbc:mysql://10.90.36.17:3306/bd_film";
+			String user = "super";
+			String password = "1111";
+
+			Class.forName(driverClassName);
+			con = DriverManager.getConnection(driverUrl, user, password);
+			return con;
+		}catch (Throwable ex) {
+			System.err.println("Initial SessionFactory creation failed." + ex);
+			throw new ExceptionInInitializerError(ex);
+		}
+	}
+	
+	public static void Connection() throws SQLException {
+		if (con==null) connectDB();        
+    }
+	public static Statement openStatement() throws SQLException {
+		if (con==null) connectDB();
+        return con.createStatement();
+    }
+	
+	
+}
