088a85560c8400bcf5883138457f28a2fab4456a
albertop10 <albertop@hotmail.es>
2018-05-21 11:54:28 +0200

    interfaz

--- ProyectoLuca/src/data/IDataFilm.java
+++ ProyectoLuca/src/data/IDataFilm.java
@@ -3,9 +3,11 @@ package data;
 import java.sql.SQLException;
 import java.util.List;
 import model.Film;
+import model.TypeGenre;
 
 public interface IDataFilm {
 
-	public List<Film> showListFilm() throws SQLException;
+	public List<Film> showListFilm(String titulo, String director ,TypeGenre genre, int year) throws SQLException;
+	public Film showFilm(int id) throws SQLException;
 
 }
